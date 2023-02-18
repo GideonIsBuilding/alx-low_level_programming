060a0beb74f940489bd867444741dad49185268a
Gideon Aleonogwe <galosikhena@gmail.com>
2023-02-17 17:41:40 -0800

    c program to print small alphabets using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet.
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int a = 97, z = 122;
+	
+	do
+	{
+		putchar(a);
+		a++;
+	}
+	while (a <= z);
+	
+	putchar('\n');
+	
+	return (0);
+}
