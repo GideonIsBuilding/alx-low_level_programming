c872698d3d62c865f6e3b40b2d5e5a7ce2bbc9e7
Gideon Aleonogwe <galosikhena@gmail.com>
2023-02-17 19:09:24 -0800

    program to run alphabets but omits q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints letter q and e.
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+        int a = 97, z = 122;
+
+	while (a <= z)
+	{
+		if ( a != 101 && a != 113)
+			putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
