7600cd8e485133ccc9c38aa4e92cdc89967074af
Gideon Aleonogwe <galosikhena@gmail.com>
2023-02-17 18:05:10 -0800

    C program that prints lowercase before uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet.
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+        int a = 97, z = 122;
+
+        do {
+                putchar(a);
+                a++;
+        } while (a <= z);
+        
+	a = 65, z = 90;
+	do {
+		putchar(a);
+		a++;
+	} while (a <= z );
+	putchar('\n');
+        return (0);
+}
