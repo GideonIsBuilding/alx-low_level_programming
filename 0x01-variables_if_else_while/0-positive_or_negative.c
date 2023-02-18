886b128889a416a346e3cb8cacf2e895757aa5e3
Gideon Aleonogwe <galosikhena@gmail.com>
2023-02-17 16:27:26 -0800

    erased whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Determines if the random generated number is positive, zero, or negative.
- *
  * Return: Always (0) Success
  */
 int main(void)
