aed3cfa44e74446cea27208455549e58691a0bf3
Gideon Aleonogwe <galosikhena@gmail.com>
2023-06-20 03:22:23 +0100

    added the file containing the functions for the C operations, the dynymic librabry file 100-operations.so, and the test pythin script.

---
+++ 0x18-dynamic_libraries/100-tests.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - A function that adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - A function that subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the subtraction.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - A function that multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - A function that divides one integer by another.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - A function that adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the modulus.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
